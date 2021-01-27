5bc4dfc58769e115989eba23a89f485f4e3ad726
1yangsh <ysh410@gmail.com>
2021-01-27 10:09:43 +0900

    news detail view update

--- news/models.py
+++ news/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
@@ -7,21 +8,26 @@ class New(models.Model):
     title = models.CharField('TITLE', max_length=300, help_text='news title')
     writer = models.CharField('WRITER', max_length=100, help_text='publisher')
     preview = models.TextField('PREVIEW', unique=True)
+    published_date = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.title
 
-    class Meta:
-        verbose_name = 'new'
-        verbose_name_plural = 'news'
-        db_table = 'news_new'
+    # def publish(self):
+    #     self.published_date = timezone.now()
+    #     self.save()
 
-    def get_absolute_url(self):
-        # -> 1st post -> /blog/post/1st-post
-        return reverse('news:news_detail', args=(self.slug,))
+    # class Meta:
+    #     verbose_name = 'new'
+    #     verbose_name_plural = 'news'
+    #     db_table = 'news_new'
 
-    def get_previous_post(self):
-        return self.get_previous_by_modify_date()
+    # def get_absolute_url(self):
+    #     # -> 1st post -> /blog/post/1st-post
+    #     return reverse('news:news_detail', args=(self.slug,))
 
-    def get_next_post(self):
-        return self.get_next_by_modify_date()
+    # def get_previous_post(self):
+    #     return self.get_previous_by_modify_date()
+
+    # def get_next_post(self):
+    #     return self.get_next_by_modify_date()
--- news/urls.py
+++ news/urls.py
@@ -5,7 +5,7 @@ app_name = 'news'
 
 urlpatterns = [
     path('', NewsLV.as_view(), name='index'),
-    path('post/<int:pk>', NewsDV.as_view(), name='news_detail'),
+    path('news/<str:pk>', NewsDV.as_view(), name='news_detail'),
     # path('search/', SearchFormView.as_view(), name='search'),
 
 ]
